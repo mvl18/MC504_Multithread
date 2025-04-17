9016e684de7d4751bbefd153a054daeb631cbea5
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2025-04-17 20:52:16 -0300

    repensando como os elfos devem agir, pois cada um deve ser uma thread

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fila.h>
 
 #define QUANT_ELFOS 10 // Quantidade de elfos no programa
 #define CHANCE_PROBLEMA                                                        \
@@ -15,20 +16,25 @@ void getHitched();
 void helpElves();
 void getHelp();
 
-// Essa função cuida do trabalho de todos os elfos. Os elfos que precisarem de
-// ajuda irão para uma fila, os que não precisarem ficarão fora da fila
-void *supervisor_elfos() {
-  // A cada 1 segundo verificamos o status de cada elfo
+fila_t fila_elfos;
+
+// O Elfo pode estar em 3 condições
+//  1. Trabalhando
+//  2. Esperando para ser ajudado
+//  3. Sendo ajudado
+void elfo() {
   while (true) {
     sleep(1);
-    for (int i = 0; i < QUANT_ELFOS; i++) {
 
-      if (random() % CHANCE_PROBLEMA == 1) {
-        //TODO:
-        // Adicionar elfo na fila
-      }
+    // Verificar se o elfo está com problema
+    if (random() % CHANCE_PROBLEMA == 1) {
+      // O elfo entra na fila
     }
   }
 }
 
-int main() {}
+int main() {
+  // Inicializando a fila de elfos
+  fila_elfos = fila_init(QUANT_ELFOS);
+
+}
