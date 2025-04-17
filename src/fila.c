62a75e072422312268432300249e24f49dd10dba
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2025-04-17 20:30:36 -0300

    Implementação inicial de uma fila simples

--- src/fila.c
+++ src/fila.c
@@ -1,22 +1,23 @@
 // TODO: finalizar isso aqui, tendo em vista as threads
 
-// Esta é uma simples implementação de fila de inteiros
-#include <stdlib.h>
+#include "fila.h"
 
-typedef struct {
-  int *buffer;
-  int inicio;
-  int fim;
-  int tamanho;
-} fila_t;
 
 fila_t fila_init(int tamanho) {
   fila_t ret = {.buffer = malloc(tamanho * sizeof(int)),
-              .inicio = 0,
-              .fim = 1,
-              .tamanho = tamanho};
+                .inicio = 0,
+                .fim = 1,
+                .tamanho = tamanho};
   return ret;
 }
 
-void fila_append(fila_t fila, int val) {
+void fila_append(fila_t *fila, uint32_t val) {
+  fila->buffer[fila->fim] = val;
+  fila->fim = (fila->fim+1)%fila->tamanho;
+}
+
+size_t fila_pop(fila_t *fila, int val) {
+  uint32_t ret = fila->buffer[fila->inicio];
+  fila->inicio = (fila->inicio+1)%fila->tamanho;
+  return ret;
 }
