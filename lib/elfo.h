9529860924df10d3ac1d08707ddcecb400ce6815
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2025-04-19 18:00:21 -0300

    Mudando tudo para não precisar de fila. Fiquei preso na parte de saber quantas threads há

--- lib/elfo.h
+++ lib/elfo.h
@@ -1,25 +1,34 @@
 #ifndef ELFO_H
 #define ELFO_H
 
-#include "fila.h"
 #include "globals.h"
+#include <limits.h>
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-fila_t *fila_elfos;
-
 sem_t semaforo_elfos; // Este semáforo indica quantos faltam para haver
                       // exatamente três elfos precisando de ajuda
+
+sem_t semaforo_fila_cheia;
 
 // TODO: Implementar no Santa
 sem_t semaforo_ajuda_finalizada; // O Santa torna o valor deste semáforo em três
                                  // para avisar os três elfos que terminou de
                                  // ajudá-los
 
-pthread_t threads_elfos[QUANT_ELFOS]; // Contém os IDs de todas as threads de elfos
+pthread_t threads_elfos[QUANT_ELFOS]; // Contém os IDs de todas as
+                                      // threads de elfos
+
+// Contém os IDs dos elfos que precisam de ajuda
+//
+// NOTE: É inicializado em zero pois os IDs dos elfos começam a contar a partir
+// de 1
+size_t elfos_precisando_de_ajuda[3] = {0};
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 // Esta função inicializa todas as threads de elfos e todos os semáforos
 // relacionados aos elfos
@@ -30,6 +39,6 @@ void elfos_close();
 
 void *elfo(void *args);
 
-void getHelp(size_t id, uint8_t posicao);
+void getHelp(size_t id);
 
 #endif // !ELFO_H
--- src/elfo.c
+++ src/elfo.c
@@ -1,6 +1,3 @@
-// NOTE: Estou parando de resolver utilizando fila porque não condiz com as
-// condições do problema.
-
 // Arquivo que contém todas as funções relacionadas aos elfos
 
 #include "elfo.h"
@@ -9,14 +6,11 @@ void elfos_init() {
   sem_init(&semaforo_elfos, NULL, 3); // O semáforo é inicializado com três
                                       // pois três elfos devem ser atendidos
                                       // ao mesmo tempo
-  sem_init(&semaforo_elfos, NULL, 0);
-
-  // Inicializando a fila de elfos
-  fila_elfos = fila_init(QUANT_ELFOS);
+  sem_init(&semaforo_ajuda_finalizada, NULL, 0);
 
   // Criar Threads de Elfos
   for (size_t i = 0; i < QUANT_ELFOS; i++) {
-    pthread_create(&threads_elfos[i], NULL, elfo, (void *)i);
+    pthread_create(&threads_elfos[i], NULL, elfo, (void *)i + 1);
   }
 }
 
@@ -25,9 +19,6 @@ void elfos_close() {
   for (size_t i = 0; i < QUANT_ELFOS; i++) {
     pthread_join(threads_elfos[i], NULL);
   }
-
-  // Destruindo a fila
-  fila_destroy(fila_elfos);
 }
 
 // O Elfo pode estar em 3 condições
@@ -43,57 +34,23 @@ void *elfo(void *args) {
     // sortear se o elfo está com problema
     if (random() % CHANCE_PROBLEMA == 1) {
 
+      // Este elfo que acabou de ficar com problema, deve esperar até que o
+      // Santa não esteja ajudando ninguém para pedir ajuda
+      sem_wait(&semaforo_elfos);
+
       printf("O elfo %d está com problema", (int)id);
-      fila_append(fila_elfos, id);
 
-      // Se houver três ou mais elfos na fila, incrementar o semáforo de elfos
-      // em três
-      if (fila_get_tamanho(fila_elfos) >= 3) { // BUG: Isso aqui tá errado
-        sem_post(&semaforo_elfos);
-        sem_post(&semaforo_elfos);
-        sem_post(&semaforo_elfos);
-      }
-
-      while (true) {
-
-        // Esse semáforo deve ser liberado pelo terceiro elfo quando entrar na
-        // fila. Apenas 3 elfos fazem a verificação por vez se ele está na
-        // frente da fila.
-        sem_wait(&semaforo_elfos);
-
-        // Verificar se ele é um dos que será atendido pelo papai noel (ou seja,
-        // se ele é o primeiro, segundo ou terceiro da fila)
-        bool foi_atendido = false;
-        for (uint8_t i = 0; i < 3; i++) {
-          if (fila_peek(fila_elfos, i)) {
-            getHelp(id, i);
-            foi_atendido = true;
-          }
-        }
-
-        // Se ele não é um dos três primeiros da fila, ele libera o semáforo
-        // para que outro elfo verifique
-        if (foi_atendido == false) {
-          sem_post(&semaforo_elfos);
-
-          // Dá um sleep de 1 segundo pra dar tempo de outras thread fazerem
-          // suas verificações
-          sleep(1);
-        }
-      }
+      // TODO: Fazer o seguinte somente quando houverem exatamente 3 elfos
+      // esperando para serem ajudados
+      // talvez usar barreira seja uma boa ideia
+      getHelp(id);
     }
   }
 }
 
 // Função executada pelo elfo quando ele está recebendo ajuda
-void getHelp(size_t id, uint8_t posicao) {
+void getHelp(size_t id) {
   printf("o elfo %d está recebendo ajuda", (int)id);
   sem_wait(&semaforo_ajuda_finalizada);
-  fila_pop(fila_elfos); // "Se retirar da fila"
-                        // NOTE:
-                        // Na verdade o que ocorre é que ele retirará po
-                        // primeiro da fila, mas como há três elfos sendo
-                        // ajudados, os três se retirarão mutualmente da fila,
-                        // fazendo com que o resultado final seja o mesmo de se
-                        // ele tivesse se retirado
+  printf("O elfo %d acabou de ser ajudado e voltará a trabalhar", (int)id);
 }
