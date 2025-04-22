cce373601f2ef5e8e1e4c7862b368a582daa14a6
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2025-04-21 23:44:21 -0300

    Acredito que as renas não precisam estar em um loop infinito

--- src/main.c
+++ src/main.c
@@ -8,12 +8,9 @@ int main() {
   elfos_init();
   reindeer_init();
   santa_init();
-  printf("1\n");
   elfos_close();
-  printf("2\n");
   reindeer_close();
-  printf("3\n");
 
-  print_green("O Programa finalizou\n");
+  print_blue("O Programa finalizou!\n");
   return 0;
 }
--- src/reindeer.c
+++ src/reindeer.c
@@ -1,6 +1,6 @@
 #include "reindeer.h"
-#include "santa.h"
 #include "log.h"
+#include "santa.h"
 
 pthread_mutex_t reindeerMutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -10,40 +10,36 @@ int reindeer_count = 0;
 
 pthread_t reindeer_threads[NUM_OF_REINDEERS];
 
-
-void getHitched(int id) {
-    print_yellow("Rena %d: Engatada ao trenó!\n", id);
-}
+void getHitched(int id) { print_yellow("Rena %d: Engatada ao trenó!\n", id); }
 
 void *reindeer(void *args) {
-    size_t id = (size_t)args;
-    while (1) {
-        sleep(rand() % 10 + 5); // Férias
-        pthread_mutex_lock(&reindeerMutex);
-        reindeer_count++;
-        print_yellow("Rena %li voltou. Total: %d\n", id, reindeer_count);
-        if (reindeer_count == NUM_OF_REINDEERS)
-            sem_post(&semaforo_acordar_santa);
-        pthread_mutex_unlock(&reindeerMutex);
+  size_t id = (size_t)args;
 
-        sem_wait(&reindeerSem);
-        getHitched(id);        
-    }
+  sleep(rand() % 10 + 5); // Férias
+  pthread_mutex_lock(&reindeerMutex);
+  reindeer_count++;
+  print_yellow("Rena %li voltou. Total: %d\n", id, reindeer_count);
+  if (reindeer_count == NUM_OF_REINDEERS)
+    sem_post(&semaforo_acordar_santa);
+  pthread_mutex_unlock(&reindeerMutex);
 
-    return NULL;
+  sem_wait(&reindeerSem);
+  getHitched(id);
+
+  return NULL;
 }
 
 void reindeer_init() {
-    sem_init(&reindeerSem, 0, 0);
+  sem_init(&reindeerSem, 0, 0);
 
-    for (size_t i = 0; i < NUM_OF_REINDEERS; i++) {
-        pthread_create(&reindeer_threads[i], NULL, reindeer, (void *)(i + 1));
-    }
+  for (size_t i = 0; i < NUM_OF_REINDEERS; i++) {
+    pthread_create(&reindeer_threads[i], NULL, reindeer, (void *)(i + 1));
+  }
 }
 
 void reindeer_close() {
-    for (int i = 0; i < NUM_OF_REINDEERS; i++) {
-        pthread_join(reindeer_threads[i], NULL);
-    }
-    pthread_mutex_destroy(&reindeerMutex);
+  for (int i = 0; i < NUM_OF_REINDEERS; i++) {
+    pthread_join(reindeer_threads[i], NULL);
+  }
+  pthread_mutex_destroy(&reindeerMutex);
 }
